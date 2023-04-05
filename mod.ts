bf0d0fc09b333ac16fb1aa89eef4604d73c3f5a5
Tisizi <git@tisizi.de>
2023-04-05 23:11:09 +0800

    Add code to project

---
+++ mod.ts
@@ -0,0 +1,87 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import { type Plugin, type Processor, unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkDirective from "remark-directive";
+import remarkGfm from "remark-gfm";
+import remarkDirectiveRehype from "remark-directive-rehype";
+import remarkRehype from "remark-rehype";
+import { createElement, Fragment, type VNode } from "preact";
+import {
+  type Comment,
+  type DocType,
+  type Element,
+  type Root,
+  type Text,
+} from "https://esm.sh/v113/@types/hast@2.3.4/index.d.ts";
+
+export type Factory<T> = (
+  props: Record<string, unknown>,
+  children: T[],
+) => Promise<T> | T;
+
+type HastNode = Root | Element | DocType | Comment | Text;
+
+type PluginArgs = [Record<string, Factory<VNode>>];
+
+const defined = (x: unknown) => x !== null && x !== undefined;
+
+const rehypePreactify: Plugin<PluginArgs, Root, VNode> = function (this, map) {
+  async function preactify(node: HastNode): Promise<VNode | null> {
+    if (node.type == "element") {
+      return preactifyElement(
+        node.tagName,
+        node.properties ?? {},
+        await preactifyMany(node.children),
+      );
+    } else if (node.type == "root") {
+      return createElement(Fragment, {}, await preactifyMany(node.children));
+    } else if (node.type == "text") {
+      return createElement(Fragment, {}, node.value);
+    } else {
+      return null;
+    }
+  }
+
+  async function preactifyMany(nodes: HastNode[]) {
+    const vnodes = await Promise.all(nodes.map(preactify));
+    return vnodes.filter(defined) as VNode[];
+  }
+
+  async function preactifyElement(
+    type: string,
+    props: Record<string, unknown>,
+    children: VNode[],
+  ) {
+    const factory: Factory<VNode> = type in map
+      ? map[type]
+      : (props, children) => createElement(type, props, children);
+    return await factory(props, children);
+  }
+
+  this.Compiler = preactify;
+};
+
+export class MarkdownPreactifier {
+  // deno-lint-ignore no-explicit-any
+  private processor: Processor<any, any, any, VNode>;
+
+  constructor(...args: PluginArgs) {
+    this.processor = unified()
+      .use(remarkParse)
+      .use(remarkGfm)
+      .use(remarkDirective)
+      .use(remarkDirectiveRehype)
+      .use(remarkRehype)
+      .use(rehypePreactify, ...args);
+  }
+
+  public async parse(markdown: string) {
+    const file = await this.processor.process(markdown);
+    return file.result;
+  }
+}
---
+++ test.tsx
@@ -0,0 +1,58 @@
+import { createElement, type VNode } from "preact";
+import { assertEquals } from "https://deno.land/std@0.182.0/testing/asserts.ts";
+import { renderToString } from "https://esm.sh/*preact-render-to-string@5.2.6";
+import { type Factory, MarkdownPreactifier } from "./mod.ts";
+
+const customElements: Record<string, Factory<VNode>> = {
+  custom: (props, children) => createElement("div", props, children),
+};
+
+async function assertParseResult(markdown: string, html: VNode) {
+  const preactifier = new MarkdownPreactifier(customElements);
+  const actual = renderToString(await preactifier.parse(markdown));
+  const expected = renderToString(html);
+  assertEquals(actual, expected);
+}
+
+async function parseHeading() {
+  await assertParseResult("# Heading", <h1>Heading</h1>);
+}
+
+async function parseInlineDirective() {
+  await assertParseResult(
+    ":custom[content]{name=0}",
+    <p>
+      <div name="0">content</div>
+    </p>,
+  );
+}
+
+async function parseBlockDirective() {
+  await assertParseResult(
+    "::custom[content]{name=0}",
+    <div name="0">content</div>,
+  );
+}
+
+async function parseContainerDirective() {
+  await assertParseResult(
+    ":::custom{name=0}\n\ncontent1\n\ncontent2\n\n:::",
+    <div name="0">
+      <p>content1</p>
+      <p>content2</p>
+    </div>,
+  );
+}
+
+async function plainText() {
+  await assertParseResult(
+    "Nothing special",
+    <p>Nothing special</p>,
+  );
+}
+
+Deno.test(parseHeading);
+Deno.test(parseInlineDirective);
+Deno.test(parseBlockDirective);
+Deno.test(parseContainerDirective);
+Deno.test(plainText);
